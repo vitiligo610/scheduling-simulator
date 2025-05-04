04d408f4be60654b6273a4fa68493c4a4cfe153f
Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>
2025-05-04 22:04:27 +0500

    make summary header sticky
    Signed-off-by: Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>

--- src/components/simulation-summary.tsx
+++ src/components/simulation-summary.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import GanttChart from "@/components/gantt-chart";
@@ -12,21 +10,25 @@ const SimulationSummary = () => {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="link">
-          View Summary
-        </Button>
+        <Button variant="link">View Summary</Button>
       </SheetTrigger>
-      <SheetContent id="simulation-summary" side="right"
-                    className="w-[900px] min-w-[900px] h-screen minimal-scrollbar overflow-y-auto">
+      <SheetContent
+        id="simulation-summary"
+        side="right"
+        className="w-[900px] min-w-[900px] h-screen minimal-scrollbar overflow-y-auto"
+      >
         <SheetHeader className="px-4 py-3">
           <div>
             <SheetTitle>Simulation Summary</SheetTitle>
             <SheetDescription>
-              Check out detailed performance metrics with process table and gantt chart.
+              Check out detailed performance metrics with chart visualizations
             </SheetDescription>
           </div>
         </SheetHeader>
-        <div className="px-4 py-3 flex flex-col gap-4 h-full overflow-y-auto minimal-scrollbar">
+
+        <div
+          className="px-4 py-3 flex flex-col gap-4 h-full overflow-y-auto minimal-scrollbar"
+        >
           <PerformanceMetrics summaryView />
           <ProcessesTable summaryView />
           <GanttChart className="w-full pl-0" />
